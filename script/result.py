8679fea6c7cd18b746314fb1e548824e0f147fb1
TaeSoo Lee <rhee92@kaist.ac.kr>
2015-05-31 23:30:06 +0900

    result

---
+++ script/result.py
@@ -0,0 +1,51 @@
+import sys
+from numpy.core.multiarray import empty
+
+if (len(sys.argv) != 2):
+    sys.exit("python eval.py input")
+else:
+    inputPath = './' + sys.argv[1]
+    outputPath = './' + sys.argv[1][:-4]+'.result.csv'
+    
+    in_f = open(inputPath,'r')
+    out_f = open(outputPath,'w')
+    in_f.readline()
+    out_f.write('AuthorId,PaperIds\n')
+    
+    front_p = []
+    mid_p = []
+    rear_p = []    
+    
+    line = in_f.readline()
+    tmp = line.split(',')
+    curr_author = tmp[0]
+    if tmp[-1] == '1':
+        front_p.append(tmp[1])
+    else:
+        mid_p.append(tmp[1])
+    
+    for line in in_f:
+        tmp = line.split(',')
+        
+        if curr_author != tmp[0]:
+            s = curr_author+","
+            s+= " ".join(front_p)+" "
+            s+= " ".join(mid_p)+" "
+            s+= " ".join(rear_p)+"\n"
+            out_f.write(s)
+            front_p[:] = []
+            mid_p[:] = []
+            rear_p[:] = []
+            curr_author = tmp[0]
+        
+        if tmp[-1] == '1':
+            if tmp[1] in front_p:
+                rear_p.append(tmp[1])
+            else:
+                front_p.append(tmp[1])
+        else:
+            mid_p.append(tmp[1])
+    
+    in_f.close()
+    out_f.close()
+    
