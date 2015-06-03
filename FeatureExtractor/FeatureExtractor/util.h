7f59a15a8492f3b84145bfe4edac2b7e9f5e72b8
secury <starjongmin@gmail.com>
2015-06-03 20:02:58 +0900

    add jaroDistnace

--- FeatureExtractor/FeatureExtractor/util.h
+++ FeatureExtractor/FeatureExtractor/util.h
@@ -7,6 +7,7 @@
 #include "db.h"
 
 unsigned int levenshteinDistance(const std::string& s1, const std::string& s2);
+double jaroDistance(const std::string &s1, const std::string &s2);
 void generateSmallDomain(DB *db);
 void stringToLower(std::string& s);
 std::vector<std::string> split(const std::string &str, const char *wschars);
